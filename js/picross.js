203ed81a4e36b0e534e6a8384a68ae614e8ae075
Violet McKinney <vi@violet.wtf>
2022-06-11 21:20:38 -0700

    Update picross.js

--- js/picross.js
+++ js/picross.js
@@ -27,13 +27,14 @@ $(function() {
 			return {
 				dimensionWidth: 10,		// default dimension width
 				dimensionHeight: 10,	// default dimension height
+				solution: [],
 				state: [],
 				hintsX: [],
 				hintsY: [],
+				mistakes: 0,
 				guessed: 0,
 				total: 100,
 				complete: false,
-				perfect: false,
 				seed: 0,
 				darkMode: false,
 				easyMode: true	// show crossouts
@@ -46,53 +47,56 @@ $(function() {
 
 		save: function() {
 			if(localStorageSupport()) {
-				localStorage['picross2.saveVersion'] = saveVersion;
+				localStorage['picross.saveVersion'] = saveVersion;
 
-				localStorage['picross2.dimensionWidth'] = JSON.stringify(this.get('dimensionWidth'));
-				localStorage['picross2.dimensionHeight'] = JSON.stringify(this.get('dimensionHeight'));
-				localStorage['picross2.state'] = JSON.stringify(this.get('state'));
-				localStorage['picross2.hintsX'] = JSON.stringify(this.get('hintsX'));
-				localStorage['picross2.hintsY'] = JSON.stringify(this.get('hintsY'));
-				localStorage['picross2.guessed'] = JSON.stringify(this.get('guessed'));
-				localStorage['picross2.total'] = JSON.stringify(this.get('total'));
-				localStorage['picross2.complete'] = JSON.stringify(this.get('complete'));
-				localStorage['picross2.perfect'] = JSON.stringify(this.get('perfect'));
-				localStorage['picross2.seed'] = JSON.stringify(this.get('seed'));
-				localStorage['picross2.darkMode'] = JSON.stringify(this.get('darkMode'));
-				localStorage['picross2.easyMode'] = JSON.stringify(this.get('easyMode'));
+				localStorage['picross.dimensionWidth'] = JSON.stringify(this.get('dimensionWidth'));
+				localStorage['picross.dimensionHeight'] = JSON.stringify(this.get('dimensionHeight'));
+				localStorage['picross.solution'] = JSON.stringify(this.get('solution'));
+				localStorage['picross.state'] = JSON.stringify(this.get('state'));
+				localStorage['picross.hintsX'] = JSON.stringify(this.get('hintsX'));
+				localStorage['picross.hintsY'] = JSON.stringify(this.get('hintsY'));
+				localStorage['picross.mistakes'] = JSON.stringify(this.get('mistakes'));
+				localStorage['picross.guessed'] = JSON.stringify(this.get('guessed'));
+				localStorage['picross.total'] = JSON.stringify(this.get('total'));
+				localStorage['picross.complete'] = JSON.stringify(this.get('complete'));
+				localStorage['picross.seed'] = JSON.stringify(this.get('seed'));
+				localStorage['picross.darkMode'] = JSON.stringify(this.get('darkMode'));
+				localStorage['picross.easyMode'] = JSON.stringify(this.get('easyMode'));
 			}
 		},
 
 		resume: function() {
 
-			if(!localStorageSupport() || localStorage['picross2.saveVersion'] != saveVersion) {
+			if(!localStorageSupport() || localStorage['picross.saveVersion'] != saveVersion) {
 				this.reset();
 				return;
 			}
 
-			var dimensionWidth = JSON.parse(localStorage['picross2.dimensionWidth']);
-			var dimensionHeight = JSON.parse(localStorage['picross2.dimensionHeight']);
-			var state = JSON.parse(localStorage['picross2.state']);
-			var hintsX = JSON.parse(localStorage['picross2.hintsX']);
-			var hintsY = JSON.parse(localStorage['picross2.hintsY']);
-			var guessed = JSON.parse(localStorage['picross2.guessed']);
-			var total = JSON.parse(localStorage['picross2.total']);
-			var complete = JSON.parse(localStorage['picross2.complete']);
-			var perfect = JSON.parse(localStorage['picross2.perfect']);
-			var seed = JSON.parse(localStorage['picross2.seed']);
-			var darkMode = JSON.parse(localStorage['picross2.darkMode']);
-			var easyMode = JSON.parse(localStorage['picross2.easyMode']);
+			var dimensionWidth = JSON.parse(localStorage['picross.dimensionWidth']);
+			var dimensionHeight = JSON.parse(localStorage['picross.dimensionHeight']);
+			var solution = JSON.parse(localStorage['picross.solution']);
+			var state = JSON.parse(localStorage['picross.state']);
+			var hintsX = JSON.parse(localStorage['picross.hintsX']);
+			var hintsY = JSON.parse(localStorage['picross.hintsY']);
+			var mistakes = JSON.parse(localStorage['picross.mistakes']);
+			var guessed = JSON.parse(localStorage['picross.guessed']);
+			var total = JSON.parse(localStorage['picross.total']);
+			var complete = JSON.parse(localStorage['picross.complete']);
+			var seed = JSON.parse(localStorage['picross.seed']);
+			var darkMode = JSON.parse(localStorage['picross.darkMode']);
+			var easyMode = JSON.parse(localStorage['picross.easyMode']);
 
 			this.set({
 				dimensionWidth: dimensionWidth,
 				dimensionHeight: dimensionHeight,
+				solution: solution,
 				state: state,
 				hintsX: hintsX,
 				hintsY: hintsY,
+				mistakes: mistakes,
 				guessed: guessed,
 				total: total,
 				complete: complete,
-				perfect: perfect,
 				seed: seed,
 				darkMode: darkMode,
 				easyMode: easyMode
@@ -122,30 +126,14 @@ $(function() {
 				}
 			}
 
-			var hintsX = this.getHintsX(solution);
-			var hintsY = this.getHintsY(solution);
-
-			this.set({
-				state: state,
-				hintsX: hintsX,
-				hintsY: hintsY,
-				guessed: 0,
-				total: total,
-				complete: false,
-				perfect: false,
-				seed: seed
-			}, {silent: true});
-			this.trigger('change');
-		},
-
-		getHintsX: function(solution) {
 			var hintsX = [];
+			var hintsY = [];
 
 			for(var i = 0; i < this.get('dimensionHeight'); i++) {
 				var streak = 0;
 				hintsX[i] = [];
 				for(var j = 0; j < this.get('dimensionWidth'); j++) {
-					if(solution[i][j] < 2) {
+					if(solution[i][j] === 1) {
 						if(streak > 0) {
 							hintsX[i].push(streak);
 						}
@@ -160,17 +148,11 @@ $(function() {
 				}
 			}
 
-			return hintsX;
-		},
-
-		getHintsY: function(solution) {
-			var hintsY = [];
-
 			for(var j = 0; j < this.get('dimensionWidth'); j++) {
 				var streak = 0;
 				hintsY[j] = [];
 				for(var i = 0; i < this.get('dimensionHeight'); i++) {
-					if(solution[i][j] < 2) {
+					if(solution[i][j] === 1) {
 						if(streak > 0) {
 							hintsY[j].push(streak);
 						}
@@ -185,111 +167,151 @@ $(function() {
 				}
 			}
 
-			return hintsY;
+			this.set({
+				solution: solution,
+				state: state,
+				hintsX: hintsX,
+				hintsY: hintsY,
+				mistakes: 0,
+				guessed: 0,
+				total: total,
+				complete: false,
+				seed: seed
+			}, {silent: true});
+			this.trigger('change');
 		},
 
 		guess: function(x, y, guess) {
+			var solution = this.get('solution')[x][y];
 			var state = this.get('state');
+			var hintsX = this.get('hintsX');
+			var hintsY = this.get('hintsY');
+			var mistakes = this.get('mistakes');
 			var guessed = this.get('guessed');
 
-			if(state[x][y] === 2) {
-				guessed--;
+			if(state[x][y] != 0) {
+				// already guessed
+				return;
 			}
 
-			if(state[x][y] === guess) {
-				state[x][y] = 0;
-			} else {
+			if(solution === guess) {
 				state[x][y] = guess;
-				if(guess === 2) {
-					guessed++;
+			} else {
+				state[x][y] = solution * -1;
+				mistakes++;
+			}
+
+			if(solution === 2) {
+				guessed++;
+			}
+
+			// cross out x -- left
+			var tracker = 0;
+			for(var i = 0; i < hintsX[x].length; i++) {
+				while(Math.abs(state[x][tracker]) === 1) {
+					tracker++;
+				}
+				if(state[x][tracker] === 0) {
+					break;
+				}
+				var streak = hintsX[x][i];
+				if(streak < 0) {
+					tracker += Math.abs(streak);
+					continue;
+				}
+				for(var j = 1; j <= streak; j++) {
+					if(Math.abs(state[x][tracker]) === 2) {
+						tracker++;
+						if(j === streak && (tracker === state[0].length || Math.abs(state[x][tracker]) === 1)) {
+							hintsX[x][i] = streak * -1;
+						}
+					} else {
+						break;
+					}
+				}
+			}
+			// cross out x -- right
+			tracker = state[0].length - 1;
+			for(var i = hintsX[x].length - 1; i >= 0; i--) {
+				while(Math.abs(state[x][tracker]) === 1) {
+					tracker--;
+				}
+				if(state[x][tracker] === 0) {
+					break;
+				}
+				var streak = hintsX[x][i];
+				if(streak < 0) {
+					tracker -= Math.abs(streak);
+					continue;
+				}
+				for(var j = 1; j <= streak; j++) {
+					if(Math.abs(state[x][tracker]) === 2) {
+						tracker--;
+						if(j === streak && (tracker === -1 || Math.abs(state[x][tracker]) === 1)) {
+							hintsX[x][i] = streak * -1;
+						}
+					} else {
+						break;
+					}
+				}
+			}
+			// cross out y -- top
+			tracker = 0;
+			for(var i = 0; i < hintsY[y].length; i++) {
+				while(Math.abs(state[tracker][y]) === 1) {
+					tracker++;
+				}
+				if(state[tracker][y] === 0) {
+					break;
+				}
+				var streak = hintsY[y][i];
+				if(streak < 0) {
+					tracker += Math.abs(streak);
+					continue;
+				}
+				for(var j = 1; j <= streak; j++) {
+					if(Math.abs(state[tracker][y]) === 2) {
+						tracker++;
+						if(j === streak && (tracker === state.length || Math.abs(state[tracker][y]) === 1)) {
+							hintsY[y][i] = streak * -1;
+						}
+					} else {
+						break;
+					}
+				}
+			}
+			// cross out y -- bottom
+			tracker = state.length - 1;
+			for(var i = hintsY[y].length - 1; i >= 0; i--) {
+				while(Math.abs(state[tracker][y]) === 1) {
+					tracker--;
+				}
+				if(state[tracker][y] === 0) {
+					break;
+				}
+				var streak = hintsY[y][i];
+				if(streak < 0) {
+					tracker -= Math.abs(streak);
+					continue;
+				}
+				for(var j = 1; j <= streak; j++) {
+					if(Math.abs(state[tracker][y]) === 2) {
+						tracker--;
+						if(j === streak && (tracker === -1 || Math.abs(state[tracker][y]) === 1)) {
+							hintsY[y][i] = streak * -1;
+						}
+					} else {
+						break;
+					}
 				}
 			}
 
 			this.set({
 				state: state,
-				guessed: guessed
-			}, {silent: true});
-
-			this.updateCrossouts(state, x, y);
-		},
-
-		updateCrossouts: function(state, x, y) {
-			var hintsX = this.get('hintsX');
-			var hintsY = this.get('hintsY');
-
-			// cross out row hints
-			var filled = true;
-			var cellIndex = 0;
-			var hintIndex = 0;
-			for(cellIndex; cellIndex < state[x].length;) {
-				if(state[x][cellIndex] === 2) {
-					if(hintIndex < hintsX[x].length) {
-						for(var i = 0; i < Math.abs(hintsX[x][hintIndex]); i++) {
-							if(state[x][cellIndex] === 2) {
-								cellIndex++;
-							} else {
-								filled = false;
-								break;
-							}
-						}
-						if(state[x][cellIndex] === 2) {
-							filled = false;
-							break;
-						}
-						hintIndex++;
-					} else {
-						filled = false;
-						break;
-					}
-				} else {
-					cellIndex++;
-				}
-			}
-			if(cellIndex < state[x].length || hintIndex < hintsX[x].length) {
-				filled = false;
-			}
-			for(var i = 0; i < hintsX[x].length; i++) {
-				hintsX[x][i] = Math.abs(hintsX[x][i]) * (filled ? -1 : 1);
-			}
-
-			// cross out column hints
-			filled = true;
-			cellIndex = 0;
-			hintIndex = 0;
-			for(cellIndex; cellIndex < state.length;) {
-				if(state[cellIndex][y] === 2) {
-					if(hintIndex < hintsY[y].length) {
-						for(var i = 0; i < Math.abs(hintsY[y][hintIndex]); i++) {
-							if(cellIndex < state.length && state[cellIndex][y] === 2) {
-								cellIndex++;
-							} else {
-								filled = false;
-								break;
-							}
-						}
-						if(cellIndex < state.length && state[cellIndex][y] === 2) {
-							filled = false;
-							break;
-						}
-						hintIndex++;
-					} else {
-						filled = false;
-						break;
-					}
-				} else {
-					cellIndex++;
-				}
-			}
-			if(cellIndex < state.length || hintIndex < hintsY[y].length) {
-				filled = false;
-			}
-			for(var i = 0; i < hintsY[y].length; i++) {
-				hintsY[y][i] = Math.abs(hintsY[y][i]) * (filled ? -1 : 1);
-			}
-
-			this.set({
 				hintsX: hintsX,
-				hintsY: hintsY
+				hintsY: hintsY,
+				mistakes: mistakes,
+				guessed: guessed
 			}, {silent: true});
 			this.trigger('change');
 		}
@@ -304,7 +326,6 @@ $(function() {
 			if(touchSupport && 'ontouchstart' in document.documentElement) {
 				return {
 					"click #new": "newGame",
-					"click #solve": "solve",
 					"change #dark": "changeDarkMode",
 					"change #easy": "changeEasyMode",
 					"mousedown": "clickStart",
@@ -322,7 +343,6 @@ $(function() {
 			} else {
 				return {
 					"click #new": "newGame",
-					"click #solve": "solve",
 					"change #dark": "changeDarkMode",
 					"change #easy": "changeEasyMode",
 					"mousedown": "clickStart",
@@ -382,10 +402,10 @@ $(function() {
 		},
 
 		_newGame: function(customSeed) {
-			$('#solve').prop('disabled', false);
 			$('#puzzle').removeClass('complete');
 			$('#puzzle').removeClass('perfect');
 			$('#progress').removeClass('done');
+			$('#mistakes').removeClass('error');
 			this.changeDimensions();
 			this.model.reset(customSeed);
 			this.render();
@@ -498,10 +518,7 @@ $(function() {
 				return;
 			}
 			
-			if (window.rightClickAnyway) {
-				e.which = 3;
-// 				this.mouseMode = 3;
-			}
+			if (window.rightClickAnyway) e.which = 3;
 
 			var target = $(e.target);
 			switch (e.which) {
@@ -533,6 +550,7 @@ $(function() {
 					break;
 			}
 			this.mouseMode = 0;
+			this.checkCompletion();
 			this.render();
 		},
 
@@ -591,60 +609,49 @@ $(function() {
 			var target = $(e.target);
 			if(Math.abs(this.mouseEndX - this.mouseStartX) < 10 && Math.abs(this.mouseEndY - this.mouseStartY) < 10) {
 				this.model.guess(target.attr('data-x'), target.attr('data-y'), 2);
+				this.checkCompletion();
 				this.render();
 			}
 		},
 
-		solve: function() {
+		checkCompletion: function() {
 			if(this.model.get('complete')) {
 				return;
 			}
 
-			var state = this.model.get('state');
-			var hintsX = this.model.get('hintsX');
-			var hintsY = this.model.get('hintsY');
+			var guessed = this.model.get('guessed');
+			var total = this.model.get('total');
 
-			var perfect = true;
-			var solutionX = this.model.getHintsX(state);
-			var solutionY = this.model.getHintsY(state);
+			if(guessed === total) {
+				var hintsX = this.model.get('hintsX');
+				var hintsY = this.model.get('hintsY');
 
-			for(var i = 0; i < hintsX.length; i++) {
-				if(hintsX[i].length !== solutionX[i].length) {
-					perfect = false;
-					break;
-				}
-				for(var j = 0; j < hintsX[i].length; j++) {
-					if(Math.abs(hintsX[i][j]) !== solutionX[i][j]) {
-						perfect = false;
-						break;
+				for(var i = 0; i < hintsX.length; i++) {
+					for(var j = 0; j < hintsX[i].length; j++) {
+						hintsX[i][j] = Math.abs(hintsX[i][j]) * -1;
 					}
 				}
-			}
-
-			for(var i = 0; i < hintsY.length; i++) {
-				if(hintsY[i].length !== solutionY[i].length) {
-					perfect = false;
-					break;
-				}
-				for(var j = 0; j < hintsY[i].length; j++) {
-					if(Math.abs(hintsY[i][j]) !== solutionY[i][j]) {
-						perfect = false;
-						break;
+				for(var i = 0; i < hintsY.length; i++) {
+					for(var j = 0; j < hintsY[i].length; j++) {
+						hintsY[i][j] = Math.abs(hintsY[i][j]) * -1;
 					}
 				}
+
+				this.model.set({
+					complete: true,
+					hintsX: hintsX,
+					hintsY: hintsY
+				});
 			}
-
-			this.model.set({
-				complete: true,
-				perfect: perfect,
-				hintsX: hintsX,
-				hintsY: hintsY
-			});
-
-			this.render();
 		},
 
 		render: function() {
+			var mistakes = this.model.get('mistakes');
+			$('#mistakes').text(mistakes);
+			if(mistakes > 0) {
+				$('#mistakes').addClass('error');
+			}
+
 			var progress = this.model.get('guessed') / this.model.get('total') * 100;
 			$('#progress').text(progress.toFixed(1) + '%');
 
@@ -655,10 +662,9 @@ $(function() {
 			}
 
 			if(this.model.get('complete')) {
-				$('#solve').prop('disabled', true);
 				$('#puzzle').addClass('complete');
-				if(this.model.get('perfect')) {
-					$('#progress').addClass('done');
+				$('#progress').addClass('done');
+				if(mistakes === 0) {
 					$('#puzzle').addClass('perfect');
 				}
 			}
@@ -669,14 +675,14 @@ $(function() {
 
 			var hintsXText = [];
 			var hintsYText = [];
-			if(this.model.get('easyMode') || this.model.get('complete')) {
+			if(this.model.get('easyMode')) {
 				for(var i = 0; i < hintsX.length; i++) {
 					hintsXText[i] = [];
 					for(var j = 0; j < hintsX[i].length; j++) {
 						if(hintsX[i][j] < 0) {
 							hintsXText[i][j] = '<em>' + Math.abs(hintsX[i][j]) + '</em>';
 						} else {
-							hintsXText[i][j] = '<strong>' + hintsX[i][j] + '</strong>';
+							hintsXText[i][j] = hintsX[i][j];
 						}
 					}
 				}
@@ -686,7 +692,7 @@ $(function() {
 						if(hintsY[i][j] < 0) {
 							hintsYText[i][j] = '<em>' + Math.abs(hintsY[i][j]) + '</em>';
 						} else {
-							hintsYText[i][j] = '<strong>' + hintsY[i][j] + '</strong>';
+							hintsYText[i][j] = hintsY[i][j];
 						}
 					}
 				}
@@ -694,13 +700,13 @@ $(function() {
 				for(var i = 0; i < hintsX.length; i++) {
 					hintsXText[i] = [];
 					for(var j = 0; j < hintsX[i].length; j++) {
-						hintsXText[i][j] = '<strong>' + Math.abs(hintsX[i][j]) + '</strong>';
+						hintsXText[i][j] = Math.abs(hintsX[i][j]);
 					}
 				}
 				for(var i = 0; i < hintsY.length; i++) {
 					hintsYText[i] = [];
 					for(var j = 0; j < hintsY[i].length; j++) {
-						hintsYText[i][j] = '<strong>' + Math.abs(hintsY[i][j]) + '</strong>';
+						hintsYText[i][j] = Math.abs(hintsY[i][j]);
 					}
 				}
 			}
@@ -712,9 +718,13 @@ $(function() {
 			}
 			html += '</tr>';
 			for(var i = 0; i < state.length; i++) {
-				html += '<tr><td class="key left">' + hintsXText[i].join('') + '</td>';
+				html += '<tr><td class="key left">' + hintsXText[i].join('&nbsp;') + '</td>';
 				for(var j = 0; j < state[0].length; j++) {
-					html += '<td class="cell s' + Math.abs(state[i][j]) + '" data-x="' + i + '" data-y="' + j + '"></td>';
+					html += '<td class="cell s' + Math.abs(state[i][j]) + '" data-x="' + i + '" data-y="' + j + '">';
+					if(state[i][j] < 0) {
+						html += 'X'; //&#9785;
+					}
+					html += '</td>';
 				}
 				html += '</tr>';
 			}
